6d98bd906e25198e0c99f4915e0edf0d272d3c77
Maksim Kushnirov <maks.kushnirov@gmail.com>
2022-07-24 20:47:26 +0300

    add spinner and error

---
+++ src/components/spinner/index.jsx
@@ -0,0 +1,7 @@
+import "./_spinner.scss";
+
+const Spinner = () => {
+    return <div className="spinner"><span class="spinner-double-section-far"></span></div>
+}
+
+export {Spinner};
--- src/pages/home-page/components/random-character/randomCharacter.jsx
+++ src/pages/home-page/components/random-character/randomCharacter.jsx
@@ -2,6 +2,8 @@ import "./_random.scss";
 import decor from "../../../../resources/img/decoration.png";
 import { Button } from "../../../../components/button";
 import { useEffect, useState } from "react";
+import { Spinner } from "../../../../components/spinner";
+import { Error } from "../../../../components/error";
 import MarvelService from "../../../../services/MarvelService";
 
 const RandomCharacter = () => {
@@ -10,37 +12,47 @@ const RandomCharacter = () => {
     const [description, setDescription] = useState();
     const [homepage, setHomepage] = useState();
     const [wiki, setWiki] = useState();
+
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
     const mv = new MarvelService();
+
+    const onError = () => {
+        setLoading(false);
+        setError(true);
+    }
+
+    const onCharacterLoaded = () => {
+        setLoading(false);
+    }
+
     const updateCharacter = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         mv.getCharacter(id)
             .then(({name, description, thumbnail, homepage, wiki}) => {
-                setName(name)
-                setThumbnail(thumbnail)
-                setDescription(description)
-                setHomepage(homepage)
-                setWiki(wiki)
+                setName(name);
+                setThumbnail(thumbnail);
+                setDescription(description);
+                setHomepage(homepage);
+                setWiki(wiki);
             })
+            .then(onCharacterLoaded)
+            .catch(onError);
     }
+
     useEffect(() => {
         updateCharacter();
-    }, [])
+    }, []);
+    const spinner = loading ? <Spinner/> : null;
+    const content = error ? <Error/> : <View name={name} 
+                                            description={description} 
+                                            thumbnail={thumbnail} 
+                                            homepage={homepage} 
+                                            wiki={wiki}/>;
     return <section className="main__random random">
-                <div className="random__section character">
-                    <div className="character__img">
-                        <img src={thumbnail} alt={name} title={name}/>
-                    </div>
-                    <div className="character__info">
-                        <h3 className="character__title">{name}</h3>
-                        <p className="character__text">
-                            {description}
-                        </p>
-                        <div className="buttons">
-                            <Button title="HOMEPAGE" href={homepage}/>
-                            <Button title="WIKI" href={wiki} secondary={true}/>
-                        </div>
-                    </div>
-                </div>
+                {spinner}
+                {content}
                 <div className="random__section description">
                     <div className="random__content">
                         <div className="random__text">
@@ -59,6 +71,24 @@ const RandomCharacter = () => {
                     </div>
                 </div>
             </section>
+}
+
+const View = ({name, description, thumbnail, homepage, wiki}) => {
+    return <div className="random__section character">
+    <div className="character__img">
+        <img src={thumbnail} alt={name} title={name}/>
+    </div>
+    <div className="character__info">
+        <h3 className="character__title">{name}</h3>
+        <p className="character__text">
+            {description}
+        </p>
+        <div className="buttons">
+            <Button title="HOMEPAGE" href={homepage}/>
+            <Button title="WIKI" href={wiki} secondary={true}/>
+        </div>
+    </div>
+</div>
 }
 
 export default RandomCharacter;
